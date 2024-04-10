157bb40ef01df2f7001fa8e6c50ef0d9e113d9bd
De-pitcher <emmanwa000@gmail.com>
2024-04-10 23:35:14 +0100

    Added comment

--- src/main/java/com/sparkz/learning/Stack.java
+++ src/main/java/com/sparkz/learning/Stack.java
@@ -1,17 +1,28 @@
 package com.sparkz.learning;
 
+/**
+ * Represents a stack data structure with fixed size.
+ * The stack supports operations such as push, pop, peek, and clearAllData.
+ */
 public class Stack {
     public int size;
+    private int stack[];
+    private int top;
 
+    /**
+     * Constructs a stack with the specified size.
+     * 
+     * @param size The maximum capacity of the stack.
+     */
     Stack(int size) {
         this.size = size;
         stack = new int[size];
         top = 0;
     }
 
-    private int stack[];
-    private int top;
-
+    /**
+     * Displays the elements of the stack.
+     */
     public void show() {
         for (int i : stack) {
             System.out.print(i + " ");
@@ -19,14 +30,27 @@ public class Stack {
         System.out.println();
     }
 
+    /**
+     * Converts the stack to an array.
+     * 
+     * @return An array representation of the stack.
+     */
     public int[] toList() {
         return stack;
     }
 
+    /**
+     * Returns the current size of the stack.
+     * 
+     * @return The number of elements in the stack.
+     */
     public int size() {
         return top;
     }
 
+    /**
+     * Clears all data from the stack.
+     */
     public void clearAllData() {
         for (int i = 0; i < stack.length; i++) {
             stack[i] = 0;
@@ -34,6 +58,11 @@ public class Stack {
         top = 0;
     }
 
+    /**
+     * Pushes an element onto the top of the stack.
+     * 
+     * @param data The element to be pushed onto the stack.
+     */
     public void push(int data) {
         if (top == size) {
             System.out.println("Stack is full");
@@ -43,6 +72,11 @@ public class Stack {
         }
     }
 
+    /**
+     * Removes and returns the element at the top of the stack.
+     * 
+     * @return The element removed from the top of the stack.
+     */
     public int pop() {
         if (isEmpty()) {
             System.out.println("Stack is empty");
@@ -56,10 +90,20 @@ public class Stack {
         }
     }
 
+    /**
+     * Returns the element at the top of the stack without removing it.
+     * 
+     * @return The element at the top of the stack.
+     */
     public int peek() {
         return stack[top - 1];
     }
 
+    /**
+     * Checks if the stack is empty.
+     * 
+     * @return true if the stack is empty, false otherwise.
+     */
     public boolean isEmpty() {
         return top <= 0;
     }
