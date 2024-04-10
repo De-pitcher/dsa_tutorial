ccda0097893166238665da0c179b8b1c47bd3a91
De-pitcher <emmanwa000@gmail.com>
2024-04-10 22:22:25 +0100

    Added comments

--- src/main/java/com/sparkz/learning/linked_list/Node.java
+++ src/main/java/com/sparkz/learning/linked_list/Node.java
@@ -1,6 +1,11 @@
 package com.sparkz.learning.linked_list;
 
+/**
+ * Represents a node in a linked list.
+ * Each node contains an integer value (data) and a reference to the next node
+ * in the list.
+ */
 public class Node {
-    int data;
-    Node next;
+    int data; // Data stored in the node
+    Node next;// Reference to the next node in the list
 }
